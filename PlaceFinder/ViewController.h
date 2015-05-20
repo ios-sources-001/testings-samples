951f728268a378ac2782940f2d51aa5952783e08
manimailinbox <Softcraft@SCS-Mac-Mini.local>
2015-05-20 15:41:39 +0530

    PlaceFinder
    This is location finder

--- PlaceFinder/ViewController.h
+++ PlaceFinder/ViewController.h
@@ -7,7 +7,11 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface ViewController : UIViewController
-
+@interface ViewController : UIViewController<CLLocationManagerDelegate>
+{
+    CLLocationManager *locationManager;
+    CLLocation *currentLocation;
+}
 @end
--- PlaceFinder/ViewController.m
+++ PlaceFinder/ViewController.m
@@ -17,8 +17,88 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    [self CurrentLocationIdentifier];
+//    [self getLocation];
 }
+
+-(void)CurrentLocationIdentifier
+{
+    //---- For getting current gps location
+    locationManager = [CLLocationManager new];
+    locationManager.delegate = self;
+    locationManager.distanceFilter = kCLDistanceFilterNone;
+    locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    [locationManager startUpdatingLocation];
+    //------
+}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
+{
+    currentLocation = [locations objectAtIndex:0];
+    [locationManager stopUpdatingLocation];
+    CLGeocoder *geocoder = [[CLGeocoder alloc] init] ;
+    [geocoder reverseGeocodeLocation:currentLocation completionHandler:^(NSArray *placemarks, NSError *error)
+     {
+         if (!(error))
+         {
+             CLPlacemark *placemark = [placemarks objectAtIndex:0];
+             NSLog(@"\nCurrent Location Detected\n");
+             NSLog(@"placemark %@",placemark);
+             NSString *locatedAt = [[placemark.addressDictionary valueForKey:@"FormattedAddressLines"] componentsJoinedByString:@", "];
+             NSString *Address = [[NSString alloc]initWithString:locatedAt];
+             NSString *Area = [[NSString alloc]initWithString:placemark.locality];
+             NSString *Country = [[NSString alloc]initWithString:placemark.country];
+             NSString *CountryArea = [NSString stringWithFormat:@"%@, %@ , %@", Area,Country,Address];
+             NSLog(@"%@",CountryArea);
+             
+             CLLocation *location = [locationManager location];
+             CLLocationCoordinate2D coordinate = [location coordinate];
+             NSString *latitude = [NSString stringWithFormat:@"%f", coordinate.latitude];
+             NSString *longitude = [NSString stringWithFormat:@"%f", coordinate.longitude];
+             NSLog(@"Latitude  = %@", latitude);
+             NSLog(@"Longitude = %@", longitude);
+         }
+         else
+         {
+             NSLog(@"Geocode failed with error %@", error);
+             NSLog(@"\nCurrent Location Not Detected\n");
+             //return;
+//             CountryArea = NULL;
+         }
+         /*---- For more results
+          placemark.region);
+          placemark.country);
+          placemark.locality);
+          placemark.name);
+          placemark.ocean);
+          placemark.postalCode);
+          placemark.subLocality);
+          placemark.location);
+          ------*/
+     }];
+}
+
+//-(void) getLocation{
+//    locationManager = [[CLLocationManager alloc] init];
+//    locationManager.delegate = self;
+//    locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+//    locationManager.distanceFilter = kCLDistanceFilterNone;
+//    [locationManager startUpdatingLocation];
+//    CLLocation *location = [locationManager location];
+//    CLLocationCoordinate2D coordinate = [location coordinate];
+//    NSString *latitude = [NSString stringWithFormat:@"%f", coordinate.latitude];
+//    NSString *longitude = [NSString stringWithFormat:@"%f", coordinate.longitude];
+//    NSLog(@"Latitude  = %@", latitude);
+//    NSLog(@"Longitude = %@", longitude);
+//}
+
+//- (void)getCurrentLocation{
+//    CLLocationCoordinate2D coordinate = [self getLocation];
+//    NSString *latitude = [NSString stringWithFormat:@"%f", coordinate.latitude];
+//    NSString *longitude = [NSString stringWithFormat:@"%f", coordinate.longitude];
+//    NSLog(@"Latitude  = %@", latitude);
+//    NSLog(@"Longitude = %@", longitude);
+//}
 
 - (void)didReceiveMemoryWarning
 {
